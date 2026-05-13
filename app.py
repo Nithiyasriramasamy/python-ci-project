e1af519c51793d2acc8b01b65c2ca13d0dceb248
Nithiyasriramasamy <nithiyasriramasamy03@gmail.com>
2026-05-13 23:25:42 +0530

    testing pipeline auto trigger

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Automatic Pipeline Trigger Successful")
+print("Simple Calculator")
+
+a = 10
+b = 5
+
+print("First Number:", a)
+print("Second Number:", b)
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("CI Pipeline Executed Successfully")
