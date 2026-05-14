51e9d02ba98c5d4257de7206b2dd322c6609a81f
Nithiyasriramasamy <nithiyasriramasamy03@gmail.com>
2026-05-14 10:05:59 +0530

    testing pipeline auto trigger

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 print("===== ONLINE FOOD ORDERING SYSTEM =====")
 
-# Food Menu
+# Menu prices
 pizza = 200
 burger = 120
 sandwich = 80
 juice = 50
 
+# Predefined orders (instead of user input)
+item1 = "Pizza"
+item2 = "Juice"
+
 total = 0
 
 print("\nMENU")
-print("1. Pizza      = ₹200")
-print("2. Burger     = ₹120")
-print("3. Sandwich   = ₹80")
-print("4. Juice      = ₹50")
+print("Pizza      = ₹200")
+print("Burger     = ₹120")
+print("Sandwich   = ₹80")
+print("Juice      = ₹50")
 
-# User Order
-item1 = input("\nEnter food item name: ")
+print("\nSelected Items:")
+print(item1)
+print(item2)
 
+# Calculate total
 if item1 == "Pizza":
     total += pizza
 elif item1 == "Burger":
@@ -25,26 +31,17 @@ elif item1 == "Sandwich":
     total += sandwich
 elif item1 == "Juice":
     total += juice
-else:
-    print("Item not available")
 
-another_order = input("Do you want another item? (yes/no): ")
+if item2 == "Pizza":
+    total += pizza
+elif item2 == "Burger":
+    total += burger
+elif item2 == "Sandwich":
+    total += sandwich
+elif item2 == "Juice":
+    total += juice
 
-if another_order == "yes":
-    item2 = input("Enter second item name: ")
-
-    if item2 == "Pizza":
-        total += pizza
-    elif item2 == "Burger":
-        total += burger
-    elif item2 == "Sandwich":
-        total += sandwich
-    elif item2 == "Juice":
-        total += juice
-    else:
-        print("Item not available")
-
-print("\nTotal amount to pay = ₹", total)
+print("\nTotal Amount = ₹", total)
 
 print("Order placed successfully")
-print("Thank you for using Online Food Ordering System")
+print("CI Pipeline Executed Successfully")
